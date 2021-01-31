c7ae968887a81bfd1e5d8f252f719a2d7f749930
Roelof Roos <github@roelof.io>
2021-01-31 16:33:22 +0100

    Fixed fixer pipeline, removed ugly fix

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,8 +4,7 @@ namespace Roelofr\SimplePayments\Tests;
 
 class ExampleTest extends TestCase
 {
-    /** @test */
-    public function true_is_true()
+    public function test_true_is_true()
     {
         $this->assertTrue(true);
     }
